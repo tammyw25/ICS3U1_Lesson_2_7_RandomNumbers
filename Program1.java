328210fa4ae9e91e3cbc042be1c46a876ee2c9da
tammyw25 <tammy.wong25@ydsbk12.ca>
2024-03-05 13:35:08 -0500

    updated Random Numbers

--- Program1.java
+++ Program1.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 class Program1 extends ConsoleProgram {
 
   /**
@@ -8,6 +10,16 @@ class Program1 extends ConsoleProgram {
   public void run() {
     // your code goes here
     
+    //creat a random object 
+    Random myRandom = new Random();
+    
+    //generate random int number and store it in a variable 
+    
+    //this generate a number 10 and 30 
+    
+    int intRandom = myRandom.nextInt(5);
+    
+    System.out.println(intRandom);
     
   }
 }
